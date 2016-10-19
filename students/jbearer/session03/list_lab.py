06d5cd1ffea46d244861566967eec929f22a08b7
heb2016 <trinity.beatrice2009@gmail.com>
2016-10-18 20:36:16 -0700

    putting another persons file back right

--- students/jbearer/session03/list_lab.py
+++ students/jbearer/session03/list_lab.py
@@ -5,11 +5,7 @@ List Lab -- October 12, 2016
 
 def fruit_tree():
     print('-> List Lab: Section 1 <-')
-<<<<<<< HEAD
-    l = ['Apples', 'Pears', 'Oranges', 'Peaches' ]
-=======
     l = [ 'Apples', 'Pears', 'Oranges', 'Peaches' ]
->>>>>>> 7fe1616ae8f466711b3ebd4cdc7f7a3c213bc7d8
     print("Original list:", l)
     
     add_fruit = input('What fruit would you like to add? ')
@@ -31,20 +27,11 @@ def fruit_tree():
             print('Starts with "P": %s' % fruit)
     
     print()
-<<<<<<< HEAD
-    print('-> List Lab: Section 2 <-')
-=======
     print('-> List Lab: Section 2 and BONUS<-')
->>>>>>> 7fe1616ae8f466711b3ebd4cdc7f7a3c213bc7d8
     print('Starting list:', l)
 
     l.pop()
     print('Last fruit removed:', l)
-<<<<<<< HEAD
-
-    del_fruit = input('Which fruit would you like to delete? ')
-    l.pop(l.index(del_fruit))
-=======
     l = l[:] * 2
     print('Multiply string * 2:',l)
     del_chk = True
@@ -56,18 +43,13 @@ def fruit_tree():
             del_chk = False
         else:
             continue
->>>>>>> 7fe1616ae8f466711b3ebd4cdc7f7a3c213bc7d8
     print('Current list:', l)
 
     print()
     print('-> List Lab: Section 3 <-')
     i = 0
     while i < len(l):
-<<<<<<< HEAD
-        answer = input('Do you like %s? ' % l[i].lower())
-=======
         answer = input('Do you like %s (yes / no)? ' % l[i].lower())
->>>>>>> 7fe1616ae8f466711b3ebd4cdc7f7a3c213bc7d8
         if answer == 'yes':
             i += 1
         elif answer == 'no':
@@ -76,9 +58,6 @@ def fruit_tree():
             continue
     print(l)
 
-<<<<<<< HEAD
-
-=======
     print()
     print('-> List Lab: Section 4 <-')
     new_l = l[:]
@@ -88,6 +67,5 @@ def fruit_tree():
     new_l.pop()
     print('Copy of original:', new_l)
     print('Reverse of copy:', rev_l)
->>>>>>> 7fe1616ae8f466711b3ebd4cdc7f7a3c213bc7d8
 
 fruit_tree()
